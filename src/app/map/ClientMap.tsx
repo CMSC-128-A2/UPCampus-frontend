8a6820bb63724b152be5ebed33302bd4e7f6e24a
jovenlab <jolabsjunior@gmail.com>
2025-04-12 21:10:16 +0800

    Edit searchbar and render to client map

--- src/app/map/ClientMap.tsx
+++ src/app/map/ClientMap.tsx
@@ -1,12 +1,20 @@
 'use client';
 
 import dynamic from 'next/dynamic';
+import SearchBar from '@/components/map/SearchBar';
 
 const Map = dynamic(() => import('@/components/map/Map'), {
     ssr: false,
     loading: () => <div>Loading map...</div>,
+    
 });
 
 export default function ClientMap() {
-    return <Map />;
+    return (
+        <div>
+            <SearchBar />  
+            <Map />
+        </div>
+    );
+
 }
--- src/components/map/SearchBar.tsx
+++ src/components/map/SearchBar.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const SearchBar = () => {
   return (
-    <div>SearchBar</div>
+    <div className="flex w-[518px] h-[69.2px] rounded-[10px] p-[10px] gap-[10px] bg-[#7F1532]">
+        SearchBar
+    </div>
   )
 }
 
