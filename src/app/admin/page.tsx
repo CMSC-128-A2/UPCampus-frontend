caaef9e233653cdc396af4b83a2a74eaf841549c
Jourdan Catarina <jourdancatarina3@gmail.com>
2025-04-13 21:47:46 +0800

    make the search bar bigger

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -70,118 +70,118 @@ function AdminPage() {
                 {/* Main Content */}
                 <div className="flex-1 bg-[#F3F3F3] overflow-y-auto">
                     <div className="m-8 bg-white border border-gray200 rounded-lg">
-                    <div className="p-6">
-                        <h1 className="text-3xl font-semibold mb-6">Science Building</h1>
+                        <div className="p-6">
+                            <h1 className="text-3xl font-semibold mb-6">Science Building</h1>
 
-                        {/* Search and Floor Selection */}
-                        <div className="mb-6">
-                            <div className="mb-4">
-                                <div className="relative">
-                                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                        <Icon icon="ph:magnifying-glass" width="20" height="20" className="text-gray-400" />
+                            {/* Search and Floor Selection */}
+                            <div className="mb-6">
+                                <div className="mb-4">
+                                    <div className="relative">
+                                        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                            <Icon icon="ph:magnifying-glass" width="20" height="20" className="text-gray-400" />
+                                        </div>
+                                        <input
+                                            type="text"
+                                            className="w-full pl-10 pr-4 py-3 text-lg border border-gray-300 rounded-lg focus:outline-none"
+                                            placeholder="Search"
+                                        />
                                     </div>
-                                    <input
-                                        type="text"
-                                        className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-1 focus:ring-green-500"
-                                        placeholder="Search"
-                                    />
+                                </div>
+
+                                <div className="flex justify-between items-center">
+                                    <button className="flex items-center px-4 py-2 bg-[#E9F2E1] text-[#93BF6A] font-medium rounded-lg border-[1.5px] text-xl border-[#93BF6A]">
+                                        <Icon icon="ph:caret-down-bold" width="20" height="20" className="mr-2" />
+                                        4th Floor
+                                    </button>
+
+                                    <button className="flex items-center px-4 py-2 bg-[#CCE8FF] text-[#4392F1] font-medium rounded-lg border-[1.5px] text-xl border-[#4392F1]">
+                                        <Icon icon="ph:plus-bold" width="20" height="20" className="mr-2" />
+                                        Schedule
+                                    </button>
                                 </div>
                             </div>
 
-                            <div className="flex justify-between items-center">
-                                <button className="flex items-center px-4 py-2 bg-[#E9F2E1] text-[#93BF6A] font-medium rounded-lg border-[1.5px] text-xl border-[#93BF6A]">
-                                    <Icon icon="ph:caret-down-bold" width="20" height="20" className="mr-2" />
-                                    4th Floor
-                                </button>
+                            {/* Class Schedules */}
+                            <div className="bg-white overflow-hidden mb-6">
+                                {/* CMSC 126 */}
+                                <div className="bg-[#CCE8FF] py-3 px-4 text-3xl">
+                                    CMSC 126
+                                </div>
 
-                                <button className="flex items-center px-4 py-2 bg-[#CCE8FF] text-[#4392F1] font-medium rounded-lg border-[1.5px] text-xl border-[#4392F1]">
-                                    <Icon icon="ph:plus-bold" width="20" height="20" className="mr-2" />
-                                    Schedule
-                                </button>
+                                <div className="overflow-x-auto">
+                                    <table className="min-w-full">
+                                        <thead>
+                                            <tr className="bg-[#F3F3F3]">
+                                                <th className="py-3 px-4 text-left font-medium text-gray-600">Section</th>
+                                                <th className="py-3 px-4 text-left font-medium text-gray-600">Type</th>
+                                                <th className="py-3 px-4 text-left font-medium text-gray-600">Room Assigned</th>
+                                                <th className="py-3 px-4 text-left font-medium text-gray-600">Schedule</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr className="border-t border-gray-300">
+                                                <td className="py-3 px-4">A</td>
+                                                <td className="py-3 px-4">Lecture</td>
+                                                <td className="py-3 px-4">SCI 405</td>
+                                                <td className="py-3 px-4">M TH | 11:00 AM - 12:00 PM</td>
+                                            </tr>
+                                            <tr className="border-t border-gray-300">
+                                                <td className="py-3 px-4">A1</td>
+                                                <td className="py-3 px-4">Laboratory</td>
+                                                <td className="py-3 px-4">SCI 402</td>
+                                                <td className="py-3 px-4">TH | 3:00 PM - 6:00 PM</td>
+                                            </tr>
+                                            <tr className="border-t border-gray-300">
+                                                <td className="py-3 px-4">A2</td>
+                                                <td className="py-3 px-4">Laboratory</td>
+                                                <td className="py-3 px-4">SCI 402</td>
+                                                <td className="py-3 px-4">M | 3:00 AM - 6:00 PM</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+
+                            {/* CMSC 129 */}
+                            <div className="bg-white overflow-hidden">
+                                <div className="bg-[#CCE8FF] py-3 px-4 text-3xl">
+                                    CMSC 129
+                                </div>
+
+                                <div className="overflow-x-auto">
+                                    <table className="min-w-full">
+                                        <thead>
+                                            <tr className="bg-[#F3F3F3]">
+                                                <th className="py-3 px-4 text-left font-medium text-gray-600">Section</th>
+                                                <th className="py-3 px-4 text-left font-medium text-gray-600">Type</th>
+                                                <th className="py-3 px-4 text-left font-medium text-gray-600">Room Assigned</th>
+                                                <th className="py-3 px-4 text-left font-medium text-gray-600">Schedule</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr className="border-t border-gray-300">
+                                                <td className="py-3 px-4">A</td>
+                                                <td className="py-3 px-4">Lecture</td>
+                                                <td className="py-3 px-4">SCI 405</td>
+                                                <td className="py-3 px-4">M TH | 9:00 AM - 10:00 AM</td>
+                                            </tr>
+                                            <tr className="border-t border-gray-300">
+                                                <td className="py-3 px-4">A1</td>
+                                                <td className="py-3 px-4">Laboratory</td>
+                                                <td className="py-3 px-4">SCI 404</td>
+                                                <td className="py-3 px-4">T | 9:00 AM - 12:00 PM</td>
+                                            </tr>
+                                            <tr className="border-t border-gray-300">
+                                                <td className="py-3 px-4">A2</td>
+                                                <td className="py-3 px-4">Laboratory</td>
+                                                <td className="py-3 px-4">SCI 404</td>
+                                                <td className="py-3 px-4">F | 9:00 AM - 12:00 PM</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
-
-                        {/* Class Schedules */}
-                        <div className="bg-white overflow-hidden mb-6">
-                            {/* CMSC 126 */}
-                            <div className="bg-[#CCE8FF] py-3 px-4 text-3xl">
-                                CMSC 126
-                            </div>
-
-                            <div className="overflow-x-auto">
-                                <table className="min-w-full">
-                                    <thead>
-                                        <tr className="bg-[#F3F3F3]">
-                                            <th className="py-3 px-4 text-left font-medium text-gray-600">Section</th>
-                                            <th className="py-3 px-4 text-left font-medium text-gray-600">Type</th>
-                                            <th className="py-3 px-4 text-left font-medium text-gray-600">Room Assigned</th>
-                                            <th className="py-3 px-4 text-left font-medium text-gray-600">Schedule</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr className="border-t border-gray-300">
-                                            <td className="py-3 px-4">A</td>
-                                            <td className="py-3 px-4">Lecture</td>
-                                            <td className="py-3 px-4">SCI 405</td>
-                                            <td className="py-3 px-4">M TH | 11:00 AM - 12:00 PM</td>
-                                        </tr>
-                                        <tr className="border-t border-gray-300">
-                                            <td className="py-3 px-4">A1</td>
-                                            <td className="py-3 px-4">Laboratory</td>
-                                            <td className="py-3 px-4">SCI 402</td>
-                                            <td className="py-3 px-4">TH | 3:00 PM - 6:00 PM</td>
-                                        </tr>
-                                        <tr className="border-t border-gray-300">
-                                            <td className="py-3 px-4">A2</td>
-                                            <td className="py-3 px-4">Laboratory</td>
-                                            <td className="py-3 px-4">SCI 402</td>
-                                            <td className="py-3 px-4">M | 3:00 AM - 6:00 PM</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-
-                        {/* CMSC 129 */}
-                        <div className="bg-white overflow-hidden">
-                            <div className="bg-[#CCE8FF] py-3 px-4 text-3xl">
-                                CMSC 129
-                            </div>
-
-                            <div className="overflow-x-auto">
-                                <table className="min-w-full">
-                                    <thead>
-                                        <tr className="bg-[#F3F3F3]">
-                                            <th className="py-3 px-4 text-left font-medium text-gray-600">Section</th>
-                                            <th className="py-3 px-4 text-left font-medium text-gray-600">Type</th>
-                                            <th className="py-3 px-4 text-left font-medium text-gray-600">Room Assigned</th>
-                                            <th className="py-3 px-4 text-left font-medium text-gray-600">Schedule</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr className="border-t border-gray-300">
-                                            <td className="py-3 px-4">A</td>
-                                            <td className="py-3 px-4">Lecture</td>
-                                            <td className="py-3 px-4">SCI 405</td>
-                                            <td className="py-3 px-4">M TH | 9:00 AM - 10:00 AM</td>
-                                        </tr>
-                                        <tr className="border-t border-gray-300">
-                                            <td className="py-3 px-4">A1</td>
-                                            <td className="py-3 px-4">Laboratory</td>
-                                            <td className="py-3 px-4">SCI 404</td>
-                                            <td className="py-3 px-4">T | 9:00 AM - 12:00 PM</td>
-                                        </tr>
-                                        <tr className="border-t border-gray-300">
-                                            <td className="py-3 px-4">A2</td>
-                                            <td className="py-3 px-4">Laboratory</td>
-                                            <td className="py-3 px-4">SCI 404</td>
-                                            <td className="py-3 px-4">F | 9:00 AM - 12:00 PM</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
                     </div>
                 </div>
             </div>
