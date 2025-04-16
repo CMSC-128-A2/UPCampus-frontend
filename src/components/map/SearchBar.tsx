ac15743f70cd05fb4f4bd09f2e98683f4c5c402d
jovenlab <jolabsjunior@gmail.com>
2025-04-16 14:03:41 +0800

    Replace SVGs with lucide-react icons and add CSS effects.

--- src/components/map/SearchBar.tsx
+++ src/components/map/SearchBar.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Image from 'next/image'
+import {Building, Goal, Search} from 'lucide-react'
 
 const SearchBar = () => {
   return (
-    <div className="flex flex-row w-[518px] h-[69.2px] rounded-[10px] p-[10px] gap-[10px] bg-[#7F1532] m-[10px]">
+    <div className="flex flex-row w-[518px] h-[69.2px] rounded-[10px] p-[10px] gap-[10px] bg-[#7F1532] m-[10px] cursor-pointer">
       <Image 
         src="/assets/images/upseelogo.png" 
         alt="Upsee Logo"
@@ -12,13 +13,7 @@ const SearchBar = () => {
       />
 
         <div className="flex items-center flex-1 bg-white text-black rounded-[8px] px-4 py-2 border-2 border-transparent focus-within:border-[#FFAE1D]">
-            <Image 
-                src="/assets/icons/lucide_search.svg" 
-                alt="Search Icon" 
-                width={20} 
-                height={20} 
-                className="mr-2"
-            />
+            <Search width={20} height={20} className='mr-2' color='#C8C8C8'/>
             <input 
                 type="text" 
                 placeholder="Search" 
@@ -26,26 +21,14 @@ const SearchBar = () => {
             />
         </div>
 
-        <div className='w-[49px] h-[49.2px] rounded-[5px] p-[10px] gap-[10px] bg-[#FFFFFF] cursor-pointer'>
-            <Image 
-                src="/assets/icons/buildings.svg" 
-                alt="Search Icon" 
-                width={40} 
-                height={40} 
-                className="mr-2"
-            />
+        <div className='group w-[49px] h-[49.2px] rounded-[5px] p-[10px] gap-[10px] bg-[#FFFFFF] cursor-pointer transform transition-transform duration-200 hover:scale-110'>
+          <Building className="w-[29px] h-[29px] text-[#7F1532] group-hover:text-[#FFAE1D]" />
         </div>
 
-        <div className='w-[49px] h-[49.2px] rounded-[5px] p-[10px] gap-[10px] bg-[#FFFFFF] cursor-pointer'>
-            <Image 
-                src="/assets/icons/activityarea.svg" 
-                alt="Activity Area Icon" 
-                width={40} 
-                height={40} 
-                className="mr-2"
-            />
+        <div className='group w-[49px] h-[49.2px] rounded-[5px] p-[10px] gap-[10px] bg-[#FFFFFF] cursor-pointer transform transition-transform duration-200 hover:scale-110'>
+          <Goal className="w-[29px] h-[29px] text-[#7F1532] group-hover:text-[#FFAE1D]" />
         </div>
-    
+
     </div>
   )
 }
