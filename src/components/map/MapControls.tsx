3a8523be00d5ef9328fc3eb6f94264d339ec20d2
cup-noodlehS <sheldonarthursagrado@gmail.com>
2025-05-29 16:16:54 +0800

    fix controls

--- src/components/map/MapControls.tsx
+++ src/components/map/MapControls.tsx
@@ -18,45 +18,29 @@ const MapControls: React.FC<MapControlsProps> = ({
     onLocate,
 }) => {
     return (
-        <div className="absolute bottom-4 left-4 flex gap-1 z-10 p-1.5 rounded-lg bg-green-accent">
-            
+        <div className="bottom-[calc(0.5rem+env(safe-area-inset-bottom))] fixed left-4 flex gap-1 z-10 p-1.5 rounded-lg bg-green-accent">
             <button
                 onClick={onZoomOut}
                 className="bg-white w-10 rounded-tl-sm rounded-bl-sm h-10 flex items-center justify-center shadow-md hover:bg-gray-100"
                 aria-label="Zoom out"
             >
-                    <Minus
-                        width="24"
-                        height="24"
-                        className="text-[#004D37]"
-                    />
+                <Minus width="24" height="24" className="text-[#004D37]" />
             </button>
 
             <button
-                        onClick={onZoomIn}
-                        className="bg-white w-10 h-10 flex items-center justify-center shadow-md hover:bg-gray-100"
-                        aria-label="Zoom in"
-                    >
-
-                        <Plus
-                                width="24"
-                                height="24"
-                                className="text-[#004D37]"
-                        />
-
+                onClick={onZoomIn}
+                className="bg-white w-10 h-10 flex items-center justify-center shadow-md hover:bg-gray-100"
+                aria-label="Zoom in"
+            >
+                <Plus width="24" height="24" className="text-[#004D37]" />
             </button>
-
 
             <button
                 onClick={onReset}
                 className="bg-white  w-10 h-10 flex items-center justify-center shadow-md hover:bg-gray-100"
                 aria-label="Reset map view"
             >
-                    <Shrink
-                        width="24"
-                        height="24"
-                        className="text-[#004D37]"
-                    />
+                <Shrink width="24" height="24" className="text-[#004D37]" />
             </button>
 
             <button
@@ -64,11 +48,7 @@ const MapControls: React.FC<MapControlsProps> = ({
                 className="rounded-tr-sm rounded-br-sm bg-white w-10 h-10 flex items-center justify-center shadow-md hover:bg-gray-100"
                 aria-label="Show my location"
             >
-                    <MapPin
-                        width="24"
-                        height="24"
-                        className="text-[#004D37]"
-                    />
+                <MapPin width="24" height="24" className="text-[#004D37]" />
             </button>
         </div>
     );
